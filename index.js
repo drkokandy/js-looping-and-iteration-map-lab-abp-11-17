bb328e91824c6b7192996742d6fb574010d0c816
Dave Kokandy <drkokandy@gmail.com>
2017-11-12 17:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(array) {
-  return array.map(array,function (driver) {
+  return array.map(function (driver) {
     return driver.toLowerCase();
   })
 }
